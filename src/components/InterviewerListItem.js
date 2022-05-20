6498eb8c3088d15cc39987916aa4ad20cb81a9bf
JarrettK <jkirck@hotmaill.com>
2022-05-20 12:53:14 -0400

    first commit

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png",
+};
+
+// should look like this
+{
+  /* <li className="interviewers__item">
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>; */
+}
