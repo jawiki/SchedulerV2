8ed7bdfeb5255dcf4621185170650c60da13cb8d
JarrettK <jkirck@hotmaill.com>
2022-05-23 13:16:32 -0400

    changes error fix interviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,24 @@
 import React from "react";
+import InterviewerListItem from "./InterviewerListItem"
+import "components/InterviewerList.scss"
 
-const interviewers = props.interviewers.map((interviewer) => {
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+       key={interviewer.id}
+       name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === value}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      />
+    );
+  });
+
   return (
-    <InterviewerListItem
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === value}
-      setInterviewer={() => onChange(interviewer.id)}
-    />
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewerList}</ul>
+    </section>
   );
-});
+}
